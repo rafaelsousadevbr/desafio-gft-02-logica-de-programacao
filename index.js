f2a5ba1366a6932e4507330d6b1fc4fdf19cb81e
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-10-05 16:22:23 -0300

    criada a função que retorna o saldo final de vitórias

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let heroWins = 30;
 
 // variável que armazena o número de derrotas
 let herodefeats = 15;
+
+//função que irá retornar o saldo final das vitórias do jogador
+const balanceOfVictories = (victories,defeats) =>{
+    return victories - defeats;
+}
